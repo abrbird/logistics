1e95abc091b8fc54176fa8030b57124cfe210b29
Bakhadyr <a.b.r.bird@gmail.com>
2022-06-19 14:57:26 +0300

    add PK

--- shard_migrations_1/20220619142912_create_orders_availability.sql
+++ shard_migrations_1/20220619142912_create_orders_availability.sql
@@ -5,7 +5,8 @@ CREATE TABLE public.logistics_orders_availability_shard_1 (
                                                order_id bigint NOT NULL,
                                                issue_point_id bigint NOT NULL,
                                                status VARCHAR NOT NULL,
-                                               updated_at TIMESTAMP NOT NULL default current_timestamp
+                                               updated_at TIMESTAMP NOT NULL default current_timestamp,
+                                               PRIMARY KEY (order_id, issue_point_id)
 );
 CREATE OR REPLACE FUNCTION trigger_set_timestamp()
     RETURNS TRIGGER AS $$
